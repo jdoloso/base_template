8484d7da242d9db55ec2ba468a24593c0dee3f32
Jason Doloso <jdoloso@exist.com>
2023-07-18 22:51:22 +0800

    Added WebAuth

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -16,7 +16,72 @@ export class DashboardComponent {
   ) {
   }
 
+  userRegistration: any;
+  defaultUser = { id: "28934jnfksjdnfjsdf", email: "jasondoloso@g.com" };
   authenticate(): void {
-    this.message.info('Authentication', 'Verifying');
+    // this.message.info('Authentication', 'Verifying');
+    this.webAuthnSignup(this.defaultUser).then(response => {
+      this.userRegistration = response;
+      console.log({response})
+      console.log(JSON.stringify(response))
+    })
   }
+
+  register(): void {
+    this.webAuthnSignin().then(response => {
+      console.log(response)
+    })
+  }
+
+  getChallenge(): any {
+    return Uint8Array.from('someChallengeIsHereComOn', c => c.charCodeAt(0))
+  }
+
+  webAuthnSignup(user: any): Promise<any> {
+    console.log('[webAuthnSignup]');
+    const publicKeyCredentialCreationOptions: PublicKeyCredentialCreationOptions = {
+      // Challenge shoulda come from the server
+      challenge: this.getChallenge(),
+      rp: {
+        name: 'WebAuthn Test',
+        // id: 'localhost:4200',
+      },
+      user: {
+        // Some user id coming from the server
+        id: Uint8Array.from(user.id, (c:any) => c.charCodeAt(0)),
+        name: user.email,
+        displayName: user.email,
+      },
+      pubKeyCredParams: [{ alg: -7, type: 'public-key' }],
+      authenticatorSelection: {
+        authenticatorAttachment: 'platform',
+        // requireResidentKey: true,
+      },
+      timeout: 60000,
+      attestation: 'direct'
+    };
+
+    return navigator.credentials.create({
+      publicKey: publicKeyCredentialCreationOptions,
+    });
+
+  }
+
+  webAuthnSignin(): Promise<any> {
+    const allowCredentials: PublicKeyCredentialDescriptor[] = [{ type: 'public-key', id: this.userRegistration?.id }];
+
+    console.log('allowCredentials', allowCredentials);
+
+    const credentialRequestOptions: PublicKeyCredentialRequestOptions = {
+      challenge: Uint8Array.from('someChallengeIsHereComOn', c => c.charCodeAt(0)),
+      allowCredentials,
+    };
+
+    console.log({credentialRequestOptions})
+
+    return navigator.credentials.get({
+      publicKey: credentialRequestOptions,
+    });
+  }
+
 }
